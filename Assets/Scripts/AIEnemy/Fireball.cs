23ecf30201abbccefc5fd323b6373f7b73818e00
Ilya <ilya.balashow1@gmail.com>
2022-02-09 12:32:17 +0600

    Added fade and scale for the fireball

--- Assets/Scripts/AIEnemy/Fireball.cs
+++ Assets/Scripts/AIEnemy/Fireball.cs
@@ -27,18 +27,21 @@ public class Fireball : MonoBehaviour
             
         }
        
-        StartCoroutine(Fade());
+        StartCoroutine(FadeAndScale());
      
 
     }
 
     
 
-    IEnumerator Fade()
+    IEnumerator FadeAndScale()
     {
         Color c = this.gameObject.GetComponent<MeshRenderer>().material.color;
-        for (float alpha = 1f; alpha >= 0; alpha -= 0.3f)
+        
+        for (float alpha = 1f, scale = 1f; alpha >= 0; alpha -= 0.5f, scale += 0.5f)
         {
+            
+           transform.localScale = new Vector3(scale, scale, scale);
             c.a = alpha;
            this.gameObject.GetComponent<MeshRenderer>().material.color = c;
             yield return new WaitForSeconds(.0001f);
